d9c46ad81063df47e60240701b91a898558ff181
Daniel Nguyen <daniel.nguyen@mrjaffesclass.com>
2023-09-05 12:46:46 -0700

    Finished Selection Sort Algorithim

--- search/Binary.java
+++ search/Binary.java
@@ -16,22 +16,22 @@ public class Binary
         int low = 0;
         int high = arr.length-1;
 
-        while (low <= high) {
-            int mid = (high + low)/2;
-            if (mid == target ){
-                target = arr[mid];
 
-            } else if (target < mid) {
+        while (low <= high) {
+            
+            int mid = (high - low)/2;
+            if (arr[mid] == target ){
+                return mid;
+
+            } 
+            
+            if (arr[mid] < target) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
-
         }
 
-        if (low > high) {
-            return -1;
-        }
         return -1;
     }
 
